8848f6cf55f94d76a0536f5a215268c14cfbb7f8
cesaltinofelix <cesaltinofelix2000@gmail.com>
2025-03-08 16:20:42 +0100

    updating code

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,12 +3,9 @@ const router = express.Router()
 const Category = require('./Category')
 const slugify = require("slugify")
 
-router.get("/admin/categories", (req, res)=>{
-    res.render("admin/categories/index")
-})
-
-router.get("/admin/categories/new", (req, res)=>{
-    res.render('admin/categories/new')
+router.get("/admin/categories", async (req, res)=>{
+    const categories = await Category.findAll();
+    res.render("admin/categories/index", {categories})
 })
 
 router.post("/categories/save", (req, res)=>{
@@ -19,11 +16,73 @@ router.post("/categories/save", (req, res)=>{
             title,
             slug: slugify(title)
         }).then(()=>{
-            res.redirect('/')
+            res.redirect('/admin/categories')
         })
     }else
     res.redirect('admin/categories/new')
 })
 
+router.post("/categories/update", (req, res)=>{
+    const id = req.body.id
+    const title = req.body.title
+    if(isNaN(id))
+        res.redirect('/admin/categories')
+    if((title != undefined) && (id != undefined))
+    {
+        Category.update({
+            title,
+            slug: slugify(title)
+        }, 
+        {
+            where: {
+                id: id
+            }
+        }
+    ).then(()=>{
+            res.redirect('/admin/categories')
+        })
+    }else
+    res.redirect('admin/categories')
+})
 
+router.post("/categories/delete", (req, res)=>{
+    const id = req.body.id
+    if(id != undefined)
+    {
+        if(!isNaN(id))
+        {
+            Category.destroy({
+                where:{
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect('/admin/categories')
+            })
+        }else
+            res.redirect('/admin/categories')
+
+    }else
+    res.redirect('/admin/categories')
+})
+
+router.get("/admin/categories/edit/:id", (req, res)=>{
+    const id = req.params.id
+    if(isNaN(id))
+        res.redirect('/admin/categories')
+    Category.findByPk(id).then((category)=>{
+        console.log(category)
+        if(category != undefined)
+        {
+            res.render("admin/categories/edit", {category})
+        }else
+        res.redirect('/admin/categories')
+    }).catch((err)=>{
+        res.redirect('/admin/categories')
+    })
+
+})
+
+router.get("/admin/categories/new", (req, res)=>{
+    res.render('admin/categories/new')
+})
 module.exports = router
